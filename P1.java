d884c3b54d0f893c093001b3692808c3c3c83e25
Aayush01055 <82150675+Aayush01055@users.noreply.github.com>
2022-12-23 13:22:30 +0000

    Commit to main

--- P1.java
+++ P1.java
@@ -1,3 +1,4 @@
+
 //Program to demonstrate use of AWT components
 import java.awt.*;
 //import java.awt.event.*;
@@ -7,7 +8,9 @@ public class P1
         Frame f = new Frame("My Frame");
         f.setSize(300,300);
         f.setVisible(true);
-        
+        f.setLayout(new FlowLayout());
+        CheckboxGroup cbg=new CheckboxGroup();
+
         Label lblname=new Label("Name");
         f.add(lblname);
 
@@ -20,13 +23,25 @@ public class P1
         TextArea txtadd=new TextArea(5,20);
         f.add(txtadd);
 
-        Checkbox chkmale=new Checkbox("Male");
-        f.add(chkmale); 
-        Checkbox chkfemale=new Checkbox("Female");
+        Label lblsubjects=new Label("Select Subjects");
+
+        f.add(lblsubjects);
+        Checkbox chkmath=new Checkbox("Math");
+        f.add(chkmath); 
+        Checkbox chkscience=new Checkbox("Science");
+        f.add(chkscience);
+
+        Label lblgender=new Label("Select Gender");
+        f.add(lblgender);
+
+        Checkbox chkmale=new Checkbox("Male",cbg,true);
+        f.add(chkmale);
+        Checkbox chkfemale=new Checkbox("Female",cbg,false);
         f.add(chkfemale);
 
         Button btnsave=new Button("Save");
         f.add(btnsave);
 
+       
     }
 }
