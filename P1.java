64bd9de3c70b1d63be8ba1ea42f7f1dd7af96095
Aayush01055 <82150675+Aayush01055@users.noreply.github.com>
2022-12-23 12:53:57 +0000

    commit to main

--- P1.java
+++ P1.java
@@ -1,1 +1,32 @@
+//Program to demonstrate use of AWT components
+import java.awt.*;
+//import java.awt.event.*;
+public class P1
+{
+    public static void main(String[] args){
+        Frame f = new Frame("My Frame");
+        f.setSize(300,300);
+        f.setVisible(true);
+        
+        Label lblname=new Label("Name");
+        f.add(lblname);
 
+        TextField txtname=new TextField(20);
+        f.add(txtname);
+
+        Label lbladd=new Label("Address");
+        f.add(lbladd);
+
+        TextArea txtadd=new TextArea(5,20);
+        f.add(txtadd);
+
+        Checkbox chkmale=new Checkbox("Male");
+        f.add(chkmale); 
+        Checkbox chkfemale=new Checkbox("Female");
+        f.add(chkfemale);
+
+        Button btnsave=new Button("Save");
+        f.add(btnsave);
+
+    }
+}
